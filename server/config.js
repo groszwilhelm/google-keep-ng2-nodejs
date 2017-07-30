b27c71a4e6efa8aa631afc82aad597ccedebe6f4
Wili Grosz <groszwilhelm@gmail.com>
2017-07-30 22:56:18 +0300

    update config file with mongo stuff

--- server/config.js
+++ server/config.js
@@ -1,6 +1,9 @@
 const config = {
     root: __dirname,
-    port: 3000
+    port: 3000,
+    mongoHost: 'localhost',
+    mongoPort: '27017',
+    mongoEndpoint: '/google-keep'
 };
 
 module.exports = config;
