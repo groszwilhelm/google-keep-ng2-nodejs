9afab2aa8211a377d0b207bcc9bcd620f79e0516
Wili Grosz <groszwilhelm@gmail.com>
2017-08-05 19:04:03 +0300

    add notification alert on delete note

--- client/src/app/components/notes/note/note.component.ts
+++ client/src/app/components/notes/note/note.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Input } from '@angular/core';
 
 import { Note } from './note.model';
 import { NotesService } from 'app/services/notes.service';
+import { NotificationsService } from 'app/notifications/notifications.service';
+import { NotificationTypes } from 'app/notifications/notifications.interface';
 
 @Component({
     selector: 'note',
@@ -11,13 +13,17 @@ import { NotesService } from 'app/services/notes.service';
 
 export class NoteComponent implements OnInit {
     @Input() note: Note;
+    private notificationsType = new NotificationTypes;
 
-    constructor(private notesService: NotesService) { }
+    constructor(private notesService: NotesService, private notificationsService: NotificationsService) { }
 
     private deleteNote(id) {
         this.notesService.delete(id)
             .subscribe(data => {
-                console.log(data.message);
+                this.notificationsService.openNotification({
+                    type: this.notificationsType.INFO,
+                    message: data.message
+                })
             })
     }
 
